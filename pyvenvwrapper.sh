ef14f7e6fa44f8e7d9a7283af60a17b8de711edb
Aman <amanraoverma@gmail.com>
2018-10-16 17:08:19 -0400

    Make pyvenvwrapper safer
    Change rmvenv to not destroy venv_home when 0 args are supplied.
    Use a custom delete function, `trash` on macOS to delete
    non-destructively. Deactivate venvs when creating other venvs.

--- pyvenvwrapper.sh
+++ pyvenvwrapper.sh
@@ -2,25 +2,31 @@
 #   Python Venv Wrapper
 ################################################################################
 
+# Variables and helper functions
 if [ "${VENV_HOME:-}" = "" ]
 then
     export VENV_HOME="$HOME/.venv" # default
 fi
 
-# create if doesn't exist
+# create venv_home if it doesn't exist
 [[ -d "$VENV_HOME" ]] || mkdir "$VENV_HOME"
 
-echoerr() {
+# command to use to trash venvs
+vw_trash() {
+    command trash "$1"
+}
+
+vw_echoerr() {
     # print to stderr
     printf "%s\n" "$*" >&2;
 }
 
 actvenv() {
+    [[ $# -eq 0 ]] && vw_echoerr "Please give the name of an environment."; return 1
     if [[ ! -d "$VENV_HOME/$1" ]]; then
-        echoerr "E: Environment '$VENV_HOME/$1' does not contain an activate script."
+        vw_echoerr "E: Environment '$VENV_HOME/$1' does not contain an activate script."
         return 1
     fi
-
     source "$VENV_HOME/$1/bin/activate"
 }
 
@@ -29,23 +35,42 @@ lsvenv() {
 }
 
 mkvenv() {
+    [[ $# -eq 0 ]] && vw_echoerr "Please give a name."; return 1
     if [[ -d "$VENV_HOME/$1" ]]; then
-        echoerr "$1 already exists in $VENV_HOME."
+        vw_echoerr "$1 already exists in $VENV_HOME."
         return 1
     fi
+    # safer to create venvs on system python only.
+    deactivate
+
     python3 -m venv "$VENV_HOME/$1"
     source "$VENV_HOME/$1/bin/activate"
     [[ $? = 0 ]] && echo "Python venv created at $VENV_HOME/$1."
 }
 
 rmvenv() {
+    [[ $# -eq 0 ]] && vw_echoerr "Please give the name of an environment."; return 1
     if [[ ! -d "$VENV_HOME/$1" ]]; then
-        echoerr "E: Environment '$VENV_HOME/$1' does not exist."
+        vw_echoerr "E: Environment '$VENV_HOME/$1' does not exist."
         return 1
     fi
     if [[ "$VIRTUAL_ENV" == "$VENV_HOME/$1" ]]; then
         deactivate
     fi
-    rm -r "$VENV_HOME/$1"
+    vw_trash "$VENV_HOME/$1"
     [[ $? = 0 ]] && echo "Python venv removed at $VENV_HOME/$1."
+}
+
+updvenv() {
+
+    [[ $# -eq 0 ]] && vw_echoerr "Please give the name of an environment."; return 1
+    if [[ ! -d "$VENV_HOME/$1" ]]; then
+        vw_echoerr "E: Environment '$VENV_HOME/$1' does not exist."
+        return 1
+    fi
+    # deactivate to make sure you are upgrading to system python.
+    deactivate
+
+    python3 -m venv --upgrade "$VENV_HOME/$1"
+    [[ $? = 0 ]] && echo "Python venv upgraded at $VENV_HOME/$1."
 }
