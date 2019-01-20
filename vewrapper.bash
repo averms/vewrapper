513bfb73e1a84c49ea03d7e38cb1593124adb907
Aman <amanraoverma@gmail.com>
2019-01-19 23:39:54 -0500

    Update conditionals in vewrapper and add tests.
    This should improve performance because `[[` is faster than `[` in bash.

--- vewrapper.bash
+++ vewrapper.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 # -----------------------------------------------------------------------------
 # Shell functions to act as wrapper for `python3 -m venv`
 # See LICENSE file for license details.
@@ -8,7 +9,7 @@
 # -----------------------------------------------------------------------------
 
 # Variables and helper functions
-if [ -z "${VENV_HOME}" ] || [ "${VENV_HOME: -1}" = '/' ]; then
+if [[ -z ${VENV_HOME} ]] || [[ ${VENV_HOME: -1} = '/' ]]; then
 	printf "%s\n%s\n" "Please set your VENV_HOME to a valid string" \
 		"It needs to be non-empty, real (not a symlink), and not end with a slash"
 	# we return non-error cause we don't want to crash the shell if -e is on
@@ -19,20 +20,22 @@ fi
 mkdir -p "$VENV_HOME"
 
 # command to use to trash venvs
-VW_RM='command trash'
+VW_rm='command trash'
 
-VW_ECHOERR() {
-    # print to stderr
-    printf "%s\n" "$*" >&2
+VW_noenvname() {
+    echo >&2 "Please give the name of an environment."
 }
 
 acve() {
-    [ $# -eq 0 ] && VW_ECHOERR "Please give the name of an environment." && return 1
-    if [ ! -d "$VENV_HOME/$1" ]; then
-        VW_ECHOERR "E: Environment '$VENV_HOME/$1' does not contain an activate script."
+    if [[ $# -eq 0 ]]; then
+        VW_noenvname
         return 1
     fi
-    . "$VENV_HOME/$1/bin/activate"
+    if [[ ! -d $VENV_HOME/$1 ]]; then
+        echo >&2 "E: Environment '$VENV_HOME/$1' does not contain an activate script."
+        return 1
+    fi
+    source "$VENV_HOME/$1/bin/activate"
 }
 
 lsve() {
@@ -40,41 +43,45 @@ lsve() {
 }
 
 mkve() {
-    [ $# -eq 0 ] && VW_ECHOERR "Please give a name." && return 1
-    if [ -d "$VENV_HOME/$1" ]; then
-        VW_ECHOERR "$1 already exists in $VENV_HOME"
+    if [[ $# -eq 0 ]]; then
+        echo >&2 "Please give a name."
+        return 1
+    fi
+    if [[ -d $VENV_HOME/$1 ]]; then
+        echo >&2 "$1 already exists in $VENV_HOME"
         return 1
     fi
     # safer to create venvs on system python only.
     deactivate 2> /dev/null
 
     python3 -m venv "$VENV_HOME/$1"
-    . "$VENV_HOME/$1/bin/activate" || return 1
+    source "$VENV_HOME/$1/bin/activate" || return 1
+    # TODO: notify user of fail here and in rmve
     pip install -U pip setuptools
     echo "Python venv created at $VENV_HOME/$1"
 }
 
 rmve() {
-    if [ $# -eq 0 ]; then
-        VW_ECHOERR "Please give the name of an environment."
+    if [[ $# -eq 0 ]]; then
+        VW_noenvname
         return 1
     fi
-    if [ ! -d "$VENV_HOME/$1" ]; then
-        VW_ECHOERR "E: Environment '$VENV_HOME/$1' does not exist."
+    if [[ ! -d $VENV_HOME/$1 ]]; then
+        echo >&2 "E: Environment '$VENV_HOME/$1' does not exist."
         return 1
     fi
-    [ "$VIRTUAL_ENV" = "$VENV_HOME/$1" ] && deactivate
-    $VW_RM "$VENV_HOME/$1" || return 1
+    [[ $VIRTUAL_ENV = $VENV_HOME/$1 ]] && deactivate
+    $VW_rm "$VENV_HOME/$1" || return 1
     echo "Python venv removed at $VENV_HOME/$1"
 }
 
 upve() {
-    if [ $# -eq 0 ]; then
-        VW_ECHOERR "Please give the name of an environment."
+    if [[ $# -eq 0 ]]; then
+        VW_noenvname
         return 1
     fi
-    if [ ! -d "$VENV_HOME/$1" ]; then
-        VW_ECHOERR "E: Environment '$VENV_HOME/$1' does not exist."
+    if [[ ! -d $VENV_HOME/$1 ]]; then
+        echo >&2 "E: Environment '$VENV_HOME/$1' does not exist."
         return 1
     fi
 
