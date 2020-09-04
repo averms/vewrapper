233ca7ffbc205e14f87da05947936b772ea9208f
Aman Verma <amanraoverma@gmail.com>
2020-09-04 17:09:30 -0400

    Tests housekeeping.

--- test/rc.bash
+++ test/rc.bash
@@ -2,10 +2,10 @@
 #   bash --init-file rc.bash
 #   . tester.bash >/dev/null 2>&1
 
-PS1='[\W] $ '
+PS1='$ '
 export VENV_HOME="$(dirname "$(realpath $0)")"/venvs
 # reset state
-gio trash "$VENV_HOME"
+rm -rf "$VENV_HOME"
 
 source ../vewrapper.bash
 # vim:ft=sh
